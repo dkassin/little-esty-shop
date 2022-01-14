8db0b92195eb02790f473463049b4953dc6e8780
David Kassin <david.j.kassin@gmail.com>
2022-01-13 17:13:04 -0700

    Test:completed story 1 features and models tests

--- spec/features/merchant/bulk_discounts/index_spec.rb
+++ spec/features/merchant/bulk_discounts/index_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe "Merchant Bulk Discount index" do
     expect(page).to have_content(@discount_2.name)
     expect(page).to_not have_content(@discount_3.name)
   end
+
+  it 'when the discount name link it clicked it goes to the discount show page' do
+    visit merchant_bulk_discounts_path(@merchant_1)
+
+    expect(page).to have_content(@discount_1.name)
+
+    click_link("Discount title: #{@discount_1.name}")
+    expect(current_path).to eq("/merchants/#{@merchant_1.id}/bulk_discounts/#{@discount_1.id}")
+  end
 end
