13083c672853e511683f34519ae4b435b5ca14e4
David Kassin <david.j.kassin@gmail.com>
2022-01-16 12:41:40 -0700

    Test: Add feature tests for bulk discount show page

---
+++ spec/features/merchants/bulk_discounts/show_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Merchant Bulk Discount show" do
+  it 'Shows a bulk discounts quantity threshold and percentage discount' do
+    visit merchant_bulk_discount_path(@merchant_1, @discount_1)
+
+    expect(page).to have_content(@discount_1.name)
+    expect(page).to have_content(@discount_1.quantity_threshold)
+    expect(page).to have_content(@discount_1.discount)
+    expect(page).to_not have_content(@discount_2.name)
+  end
+end
