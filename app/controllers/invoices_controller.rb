1044b6e1644570f86c42decb0c1f8f12dbccde93
David Kassin <david.j.kassin@gmail.com>
2022-01-19 00:08:42 -0700

    final test

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,6 +7,6 @@ class InvoicesController < ApplicationController
 
   def show
     @merchant = Merchant.find(params[:merchant_id])
-    @invoice = Invoice.find(params[:id])
+    @invoice = @merchant.invoices.find(params[:id])
   end
 end
